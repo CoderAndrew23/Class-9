5e83baccb6e370048b447a2ab32d25e7304c1e3d
CoderAndrew <109491024+Icybits34@users.noreply.github.com>
2022-09-10 10:20:30 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,72 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-function setup() {
-  createCanvas(400,400);
-  background(51);
-  box = createSprite(200,200,30,30);
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var arrow;
+var baseimage;
+var playerimage;
 
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
 }
 
-function draw() 
-{
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
 
-  // write the code to change background color 
-  // to red when RIGHT_ARROW is pressed
-  
-  if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
+  engine = Engine.create();
+  world = engine.world;
+
+  angleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
+
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
+
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.position.y - 112,
+    120,
+    120
+  );
+
+  arrow = new PlayerArrow(
+    playerArcher.body.position.x,
+    playerArcher.body.position.y,
+    100,
+    10
+  );
+}
+
+function draw() {
+  background(backgroundImg);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+  Engine.update(engine);
+
+  playerArcher.display();
+  arrow.display();
+
+  if (keyCode === 32) {
+    
+     arrow.shoot(playerArcher.body.angle);
     
   }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
-   
-  }
 
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
-
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    background("red");
-  }
-
-  drawSprites();
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
 }
